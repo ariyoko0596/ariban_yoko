6cdb42e4186606b83b176872bad20295f66a82cb
ariyoko0596 <ari.yoko0596@gmail.com>
2026-07-27 21:22:48 +0700

    fix: update astro config with vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,7 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
-import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  // URL utama situs Anda untuk keperluan sitemap (ubah sesuai domain produksi Anda nanti)
-  site: 'https://aribanyoko.com',
-
-  // Daftarkan sitemap ke dalam integrations
-  integrations: [sitemap()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: vercel(),
 });
