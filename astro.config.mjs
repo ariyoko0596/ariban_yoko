2af19f62b592cf9329ee17806f67ba25de8c8c4c
ariyoko0596 <ari.yoko0596@gmail.com>
2026-07-27 21:24:40 +0700

    fix: correct vercel adapter import in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'; // <-- Cukup import dari '@astrojs/vercel' saja
 
 export default defineConfig({
   output: 'server',
